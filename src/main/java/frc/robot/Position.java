c42d9c02d3d9a2ce311b4b3dae92bd738cc8a4c4
SophiaJAVA <107653067+SophiaJAVA@users.noreply.github.com>
2022-06-30 23:15:59 +0000

    grid robot test passed

--- src/main/java/frc/robot/Position.java
+++ src/main/java/frc/robot/Position.java
@@ -5,28 +5,40 @@ import static lib.Romi.turnDegrees;
 
 public class Position {
 
+    int x;
+    int y;
+
+  
+
 
     public void driveUp() throws InterruptedException {
-
+        y += 10;
     }
 
     public void driveDown() throws InterruptedException {
-
+         y -= 10;
     }
 
     public void driveRight() throws InterruptedException {
-
+          x += 10;
     }
 
     public void driveLeft() throws InterruptedException {
-
+          x -= 10;
     }
 
     public void returnToHome() {
 
+          x = 0;
+          y = 0;
+
     }
 
     public String toString() {
-        return "";
+        return "("+x+", "+y+")";
+        // x = 10, y = 20
+        // (10, 20)
+      
+
     }
 }
